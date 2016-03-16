7495b7303c7a6bef951622d552335ff0367da278
Igor Cuiumju <cuiumjuigor@gmail.com>
2016-03-16 17:30:35 +0200

    [Task 2.4] refactored the validation

--- task 2/Task 2.4/js/validator.js
+++ task 2/Task 2.4/js/validator.js
@@ -1,10 +1,12 @@
-function validate() {
+
+$(document).ready(function() {
+    $("#btnValidate").click(function(){
+ 
 	var name = $("#inName").val();
 	var email = $("#inEmail").val();
 	var subject = $("#inSubject").val();
 	var message = $("#inMessage").val();
 
-	var isValid = true;
 	
 	if (name.length >= 3) {
 		$('#inName').addClass("valid");
@@ -13,17 +15,15 @@ function validate() {
 	else {
 		$('#inName').addClass("error");
 		$("#inName").removeClass("valid");
-		isValid = false;
 	}
 	
-		if (email.length > 0 && validateEmail(email)) {
+	if (email.length > 0 && validateEmail(email)) {
 		$('#inEmail').addClass("valid");
 		$("#inEmail").removeClass("error");
 	}
 	else {
 		$('#inEmail').addClass("error");
 		$("#inEmail").removeClass("valid");
-		isValid = false;
 	}
 
 	if (subject.length >= 10) {
@@ -33,7 +33,6 @@ function validate() {
 	else {
 		$('#inSubject').addClass("error");
 		$("#inSubject").removeClass("valid");
-		isValid = false;
 	}
 	
 	if (message.length >= 10 && message.length < 201) {
@@ -43,9 +42,8 @@ function validate() {
 	else {
 		$('#inMessage').addClass("error");
 		$("#inMessage").removeClass("valid");
-		isValid = false;
 	}
-}	
+	
 
 
 function validateEmail(email) {
@@ -53,3 +51,5 @@ function validateEmail(email) {
 	return re.test(email);
 }
 
+   }); 
+});
